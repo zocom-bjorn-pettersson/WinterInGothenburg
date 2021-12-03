a40f90966dae70117553c1b6238f4629581a5a86
Björn Pettersson <bjorn.pettersson@zocom.se>
2021-12-03 13:08:03 +0100

    Lade till sout om att det är från branch bjorn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("WinterInGothenburg!");
+        System.out.println("Det här är från branch bjorn");
     }
 }
