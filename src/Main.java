6a725994118e1433f3ad962b0c7678f140d4b4db
sheeba <rl.sheebanair@gmail.com>
2021-12-03 13:50:44 +0100

    Sheeba branch commit of WinterIn Gothenburg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("WinterInGothenburg!");
+        System.out.println("This is a new branch known as Sheeba");
     }
 }
